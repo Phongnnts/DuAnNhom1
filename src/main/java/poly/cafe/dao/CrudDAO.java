1182c6d444e93c3e41298076e150e118b7369972
nguye <nguye@KhiNguyn>
2025-05-12 09:13:08 +0700

    daxong

--- src/main/java/poly/cafe/dao/CrudDAO.java
+++ src/main/java/poly/cafe/dao/CrudDAO.java
@@ -4,10 +4,26 @@
  */
 package poly.cafe.dao;
 
+import java.util.List;
+import poly.cafe.entity.Category;
+
 /**
  *
- * @author admin
+ * @author LENOVO
+ 
  */
-public interface CrudDAO {
-    
+public abstract class CrudDAO<EntityType, KeyType> {
+
+    public abstract void insert(EntityType entity);
+
+    public abstract void update(EntityType entity);
+
+    public abstract void delete(KeyType id);
+
+    public abstract List<EntityType> selectAll();
+
+    public abstract EntityType selectedByID(KeyType id);
+
+    public abstract List<EntityType> selectBySQL(String sql, Object... args);
 }
+
